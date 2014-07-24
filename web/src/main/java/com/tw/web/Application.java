c172c09df91059f615bd50dc94f50f58491c9252
Zhaoyu Wei <weixiaobo88@gmail.com>
2014-07-24 20:55:20 +0800

    zy: add Application.java back for spring boot

---
+++ web/src/main/java/com/tw/web/Application.java
@@ -0,0 +1,14 @@
+package com.tw.web;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by twer on 7/24/14.
+ */
+public class Application {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
