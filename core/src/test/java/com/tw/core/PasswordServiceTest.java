f7d49c1f883f1ebac122968bd90f0c1ab1b27faf
Zhaoyu Wei <weixiaobo88@gmail.com>
2014-07-31 13:53:03 +0800

    zy: change password service test to use password service

--- core/src/test/java/com/tw/core/PasswordServiceTest.java
+++ core/src/test/java/com/tw/core/PasswordServiceTest.java
@@ -6,14 +6,18 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by twer on 7/31/14.
+ * Created by twer on 7/30/14.
  */
-public class PasswordServiceTest {
-    @Test
-    public void when_encrypt_should_return_encrypted_string() {
-        String password = "hello";
-        String encrptedPassword = "5d41402abc4b2a76b9719d911017c592";
 
-        assertEquals(encrptedPassword, new PasswordService().encrypt(password));
+public class PasswordServiceTest {
+
+    @Test
+    public void when_encode_password_with_md5_should_return_encrypted_password() {
+        PasswordService passwordService = new PasswordService();
+        String password = "hello";
+        String hashedPassword = passwordService.encrypt(password);
+
+        assertEquals("5d41402abc4b2a76b9719d911017c592", hashedPassword);
     }
+
 }
